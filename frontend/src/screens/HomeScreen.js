5c858c725e0b286822f872fd3fa720fe07e53cc9
Tatsuro <contacts@tatsuro.dev>
2020-10-20 12:10:48 +0900

    products.jsからではなくbackend apiからproductsを取得、hooksを使ってcomponentに表示 12
    npm i axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  // stateless functional componentはstateを持つことができないが、hooksを使うことでreact側にstateを保持させることで、擬似的にstateを導入できる
+  // useState(initialState)でstate, stateをsetする関数がarrayで返ってくる。initialStateのproductsは複数なのでempty arrayを指定
+  const [products, setProducts] = useState([])
+  // conmpnentのrendering後に実行されるhook、lifecycle methodのようなもの
+  useEffect(() => {
+    // useEffectの第一引数の関数にasyncを指定できないので、中で別の関数にasync関数を作成
+    const fetchProducts = async () => {
+      // status, dataなどのkeyを持つresponseが返ってくる
+      const { data } = await axios.get('/api/products')
+      // products stateにdataをset
+      setProducts(data)
+    }
+    fetchProducts()
+    // 第二引数に与えられたstateに変化があったときに、第一引数の関数が実行される
+    // 第2引数を指定なし => Render毎(state, propsのうちいずれかが更新される度)に第１引数の関数を実行。
+    // 第2引数に[]を指定 => mount, unmount時に第１引数の関数を実行。
+    // 第2引数に値の配列を指定 => mount時と指定された値に変化があった場合のみに第１引数の関数を実行。
+  }, [])
   return (
     <>
       <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 // Route componentにより、props.match, location, historyなどのpropsが渡される
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id)
+  // initialStateのproductは単数のempty object
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+    // match.params.idが変わる度にrerender
+  }, [match])
   return (
     <>
       <Link className="btn btn-light" to="/">Go Back</Link>
