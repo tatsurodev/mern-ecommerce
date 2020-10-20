8f9b681290155929ed9ea7910896007a495ecadc
Tatsuro <contacts@tatsuro.dev>
2020-10-20 22:06:24 +0900

    users data作成 20
    npm i bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+// bcryptよりbcryptjsの方がdependenciesが少ない、
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('password', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('password', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('password', 10),
+  },
+]
+
+export default users
