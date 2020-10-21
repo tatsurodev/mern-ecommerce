bc1e8110d478f6b85a2093ed22ded61cd57a073c
Tatsuro <contacts@tatsuro.dev>
2020-10-22 04:17:12 +0900

    HomeScreenからlistProducts actionをdispatch 28

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,40 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
+// reduxからstate取得してrenderまでの流れ
+// dispatch(listProducts action) -> useSelectorでproductList state取得 -> render
 const HomeScreen = () => {
+  // actionをdispatchするため
+  const dispatch = useDispatch()
+  // useSelectorでstoreからstateを取得
+  const productList = useSelector(state => state.productList)
+  // productList reducerでsetされるstateを分割代入で取得
+  const { loading, error, products } = productList
   // stateless functional componentはstateを持つことができないが、hooksを使うことでreact側にstateを保持させることで、擬似的にstateを導入できる
-  // useState(initialState)でstate, stateをsetする関数がarrayで返ってくる。initialStateのproductsは複数なのでempty arrayを指定
-  const [products, setProducts] = useState([])
   // conmpnentのrendering後に実行されるhook、lifecycle methodのようなもの
   useEffect(() => {
-    // useEffectの第一引数の関数にasyncを指定できないので、中で別の関数にasync関数を作成
-    const fetchProducts = async () => {
-      // status, dataなどのkeyを持つresponseが返ってくる
-      const { data } = await axios.get('/api/products')
-      // products stateにdataをset
-      setProducts(data)
-    }
-    fetchProducts()
+    dispatch(listProducts())
     // 第二引数に与えられたstateに変化があったときに、第一引数の関数が実行される
     // 第2引数を指定なし => Render毎(state, propsのうちいずれかが更新される度)に第１引数の関数を実行。
     // 第2引数に[]を指定 => mount, unmount時に第１引数の関数を実行。
     // 第2引数に値の配列を指定 => mount時と指定された値に変化があった場合のみに第１引数の関数を実行。
-  }, [])
+  }, [dispatch])
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col
-            key={product._id}
-            sm={12} md={6} lg={4} xl={3}
-          >
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {
+        loading ? (
+          <h2>Loading...</h2>
+        ) :
+          error ? (
+            <h3>{error}</h3>
+          ) : (
+              <Row>
+                {products.map(product => (
+                  <Col
+                    key={product._id}
+                    sm={12} md={6} lg={4} xl={3}
+                  >
+                    <Product product={product} />
+                  </Col>
+                ))}
+              </Row>
+            )
+      }
     </>
   )
 }
