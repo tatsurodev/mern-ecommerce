b79680c900aa094ac08f5effdbaafe154c1ce629
Tatsuro <contacts@tatsuro.dev>
2020-10-24 04:51:38 +0900

    cartConstants, cartReducer, cartAction作成 32

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,21 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 // reducers
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  }
+}
 
 const middleware = [thunk,]
 
