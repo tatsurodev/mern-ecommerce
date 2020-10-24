40fc88db68f72dd7cd7e0aece36823d6d5d2f5df
Tatsuro <contacts@tatsuro.dev>
2020-10-24 15:19:59 +0900

    router内の処理をcontrollerへ移動 36

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 const router = express.Router()
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-  // throw new Error('Error!!!')
-  // json形式でresを返す
-  res.json(products)
-}))
-
-// @desc    Fetch single product
-// @route   GET /api/product/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-  // dynamic paramsには, req.params.paramNameでaccess
-  const product = await Product.findById(req.params.id)
-  if (product) {
-    res.json(product)
-  } else {
-    // productが見つからない時404のerrorをthrow
-    res.status(404)
-    throw new Error('Product not found')
-  }
-}))
+// router.get('/', getProducts)は下記と同値
+// router.route(path)は複数のmethodを追加できる ex.router.route(path).get().post().delete()
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
