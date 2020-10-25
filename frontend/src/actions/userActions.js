1270d611472bab9b3fce89bc8aa22a56eeb704eb
Tatsuro <contacts@tatsuro.dev>
2020-10-25 20:43:00 +0900

    Header componentでlogin user表示 45

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGIN_FAIL, USER_LOGOUT,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
 } from '../constants/userConstants'
 
 // formからemail, password取得、
@@ -37,4 +38,9 @@ export const login = (email, password) => async (dispatch) => {
         error.message,
     })
   }
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
 }
