9692a98171ddd77522ece4961714aa23eec65076
Tatsuro <contacts@tatsuro.dev>
2020-10-27 15:58:53 +0900

    ProfileScreenでuesrDetails取得 48

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { getUserDetails } from '../actions/userActions'
+
+const ProfileScreen = ({ location, history }) => {
+  // component level state
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState('')
+  // reduxからstate取得
+  const dispatch = useDispatch()
+  // reduxのuserDetails state
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+  // reduxのuserLogin stateを取得して未loginのuserはaccessさせない
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  // 未loginでredirect
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    } else {
+      // login済だが、userDetails stateがまだない
+      // !user.nameの代わりに、Object.keys(user).length === 0 && user.constructor === Objectでuserが{}かどうかで判定してもおｋ
+      if (!user.name) {
+        // /api/users/profileへのgetでauthMiddlewareでreq.userにaccessしているuserがsetされる
+        dispatch(getUserDetails('profile'))
+      } else {
+        // login済、useDetails取得済なので、component level stateにname, emailをset
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, history, userInfo, user])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      setMessage(null)
+      // dispatch update profile
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>Usre Profile</h2>
+        {message && <Message variant="danger">{message}</Message>}
+        {error && <Message variant="danger">{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
