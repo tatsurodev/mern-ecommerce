682c990871afac55ae6d006ccd11dff8182e63ae
Tatsuro <contacts@tatsuro.dev>
2020-11-03 03:23:06 +0900

    paypal決済機能追加 63

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 // 404 not found
 app.use(notFound)
 // error handlerでerror時の処理を記述、全routingの後に設定
